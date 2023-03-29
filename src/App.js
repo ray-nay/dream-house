fdb53c8c4f5313bdee00e5c3c0fa8a7291873c37
Renee Mumbi <1723renee@gmail.com>
2023-03-29 22:47:02 +0300

    Update app.js component with login functionality

--- src/App.js
+++ src/App.js
@@ -7,11 +7,24 @@ import Form from './Components/Form';
 import ViewMore from './Components/ViewMore';
 import NavBar from './Components/NavBar';
 import Search from './Components/Search';
-
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
 function App() {
   
   const [houses, setHouses] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [user, setUser] = useState(null);
+  
+  // const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+
 
   useEffect(() => {
     fetch("http://localhost:9292/houses")
@@ -22,7 +35,7 @@ function App() {
   // const displayedHouses = houses.filter((house) => {
   //   return house.name.toLowerCase().includes(searchTerm.toLowerCase())
   // });
-   
+  // if (!user) return <Login onLogin={setUser} />;
   return (
 
     
@@ -34,6 +47,8 @@ function App() {
           <Route path="/" element={<Home houses={houses}/>} />
           <Route path="/form" element={<Form houses={houses} setHouses={setHouses} />}/>
           <Route path="/house/:id" element={<ViewMore />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/signup" element={<SignUp setUser={setUser} />} />
         </Routes>
         
       </div>
